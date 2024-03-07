930cc473d6b00ff2cd157d7e19148801446734b1
Youri Ran <y.a.ran@uva.nl>
2024-03-07 13:19:42 +0100

    define __cplusplus

--- setup.py
+++ setup.py
@@ -3,12 +3,14 @@ from setuptools import setup
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 import platform
 
-std_version = "/std:c++17" if platform.system() == "Windows" else "-std=c++17"
+# MSVC, compiler used in conda uses different flag for the c++ version and does not define the __cplusplus variable by default
+# set it so that the breakthrough video chmods don't break (Windows also doesn't have S_IRWXU)
+std_version = ["/std:c++17", '/Zc:__cplusplus'] if platform.system() == "Windows" else ["-std=c++17"]
 ext_modules = [
     Pybind11Extension(
     "_ruptura",
     sources=glob("src/*.cpp"),
-    extra_compile_args= [std_version, "-DPYBUILD=1", "-D_LIBCPP_DISABLE_AVAILABILITY"]
+    extra_compile_args= std_version + ["-DPYBUILD=1", "-D_LIBCPP_DISABLE_AVAILABILITY"]
     )
 ]
 
